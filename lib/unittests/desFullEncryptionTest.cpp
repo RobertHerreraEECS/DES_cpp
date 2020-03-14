94c33bd6d46e4b525a193870c99596ed5d2caa4c
Robert Herrera <dev.robertherrera.0070@gmail.com>
2020-03-13 19:54:27 -0700

    Setting up unit test envrionment

---
+++ lib/unittests/desFullEncryptionTest.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+//#include "Formula.h"
+
+TEST(encryptionTest, test1) {
+    //arrange
+    //act
+    //assert
+    EXPECT_EQ (0,  0);
+    EXPECT_EQ (0,  0);
+}
+
+TEST(decryptionTest, test2) {
+    //arrange
+    //act
+    //assert
+    EXPECT_EQ (0,  0);
+    EXPECT_EQ (0,  0);
+}

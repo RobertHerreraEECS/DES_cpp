fe02b364e989857b0c5829c2e990d95a8519c308
Robert Herrera <dev.robertherrera.0070@gmail.com>
2020-03-15 20:02:56 -0700

    Updated and validated tests

--- lib/encryption.cpp
+++ lib/encryption.cpp
@@ -26,9 +26,6 @@ std::vector<uint64_t> DesEncryption::ullFromString(std::string a){
     size = (int) a.size() / 8;
     remainder = a.size() % 8;
 
-
-    std::cout << a << std::endl;
-
     std::vector<int> dataChunks;
     for (int i = 0; i < size; i++) {
     	dataChunks.push_back(8);
@@ -44,7 +41,7 @@ std::vector<uint64_t> DesEncryption::ullFromString(std::string a){
         const char * temp = &(a.substr(i,c).c_str()[0]);
         memcpy(charData.raw,temp,sizeof(uint64_t));
         text[j] = charData.data;
-        ullVector.push_back(charData.data);
+        ullVector.push_back(__builtin_bswap64 (text[j]));
         i+=8;
         j++;
     }
@@ -59,9 +56,9 @@ std::string DesEncryption::decryptData (std::vector<uint64_t> encryptedData,
 
     for (int i = 0; i < len; i++) {
     	Char_Int64 intData;
-    	intData.data = ptext[i];
+    	intData.data = __builtin_bswap64 (ptext[i]);
     	std::string s = intData.raw;
-    	out += s;
+    	out += s.substr(0,8);
     }
     free(ptext);
     return out;
@@ -71,6 +68,7 @@ std::vector<uint64_t> DesEncryption::encryptData (std::string a, uint64_t key){
 
     std::vector<uint64_t> encryptedData;
     auto plaintext = ullFromString(a);
+
 	int len = plaintext.size();
     uint64_t *ciphertext = ::desEncryptECB(plaintext.data(),len,key);
 
--- lib/unittests/desFullEncryptionTest.cpp
+++ lib/unittests/desFullEncryptionTest.cpp
@@ -22,7 +22,6 @@ TEST(singleEncryptTest, singleChunk1) {
     uint64_t key = 0x133457799BBCDFF1;
     uint64_t message = 0x0123456789ABCDEF;
     uint64_t ptext = ::_encrypt(message,key);
-    std::cout << "[*] c: 0x" << std::hex << ptext << std::endl;
     EXPECT_EQ (C,  ptext);
 }
 
@@ -31,7 +30,6 @@ TEST(singleEncryptTest, singleChunk2) {
     uint64_t key = 0x0E329232EA6D0D73;
     uint64_t message = 0x8787878787878787;
     uint64_t ptext = ::_encrypt(message,key);
-    std::cout << "[*] c: 0x" << std::hex << ptext << std::endl;
     EXPECT_EQ (C,  ptext);
 }
 
@@ -41,7 +39,6 @@ TEST(singleDecryptTest, singleChunk1) {
     uint64_t key = 0x133457799BBCDFF1;
     uint64_t plaintext = 0x0123456789ABCDEF;
     uint64_t ptext = ::_decrypt(message,key);
-    std::cout << "[*] c: 0x" << std::hex << ptext << std::endl;
     EXPECT_EQ (plaintext,  ptext);
 }
 
@@ -50,7 +47,6 @@ TEST(singleDecryptTest, singleChunk2) {
     uint64_t key = 0x0E329232EA6D0D73;
     uint64_t plaintext = 0x8787878787878787;
     uint64_t ptext = ::_decrypt(message,key);
-    std::cout << "[*] c: 0x" << std::hex << ptext << std::endl;
     EXPECT_EQ (plaintext,  ptext);
 }
 
@@ -70,17 +66,13 @@ TEST(encryptionTest, test1) {
     ciphertext[0] = 0xC0999FDDE378D7ED;
     ciphertext[1] = 0x727DA00BCA5A84EE; 
     ciphertext[2] = 0x47F269A4D6438190; 
-    ciphertext[3] = 0x9DD52F78F5358499;
+    ciphertext[3] = 0xD9D52F78F5358499; // typo in documentation
     ciphertext[4] = 0x828AC9B453E0E653;
 
     uint64_t * ctext = ::desEncryptECB(a, 5, key);
 
-    // TODO: add zero padding
-    // investigate issue with 64 (4)
     for (int i = 0; i < 5; i++) {
-    	//EXPECT_EQ (ctext[i],ciphertext[i]);
-    	std::cout << "c: " << std::hex << 
-    	ctext[i] << " = " << ciphertext[i] << std::endl;
+    	EXPECT_EQ (ctext[i],ciphertext[i]);
     }
 }
 
@@ -96,26 +88,62 @@ TEST(decryptionTest, test1) {
 
     uint64_t key = 0x0E329232EA6D0D73;
 
-    //C0999FDDE378D7ED 727DA00BCA5A84EE 47F269A4D6438190 9DD52F78F5358499 828AC9B453E0E653
+    uint64_t ciphertext[5] = {0};
+    ciphertext[0] = 0xC0999FDDE378D7ED;
+    ciphertext[1] = 0x727DA00BCA5A84EE; 
+    ciphertext[2] = 0x47F269A4D6438190; 
+    ciphertext[3] = 0xD9D52F78F5358499; // // typo in documentation
+    ciphertext[4] = 0x828AC9B453E0E653;
 
-    uint64_t * ctext = ::desEncryptECB(a, 5, key);
+    uint64_t * ptext = ::desDecryptECB(ciphertext, 5, key);
 
     for (int i = 0; i < 5; i++) {
-    	std::cout << std::hex << ctext[i] << std::endl;
+        EXPECT_EQ (ptext[i],  a[i]);
     }
-    //assert
-    EXPECT_EQ (0,  0);
 }
 
 // C++ Wrapper layer
 TEST(fullEncryptionTest, test1) {
 
+    uint64_t ciphertext[5] = {0};
+    ciphertext[0] = 0xC0999FDDE378D7ED;
+    ciphertext[1] = 0x727DA00BCA5A84EE; 
+    ciphertext[2] = 0x47F269A4D6438190; 
+    ciphertext[3] = 0xD9D52F78F5358499; // // typo in documentation
+    ciphertext[4] = 0x828AC9B453E0E653;
+
+    // TODO: add zero padding
+    std::string a = "Your lips are smoother than vaseline\r\n";
+
     DesEncryption e;
-    EXPECT_EQ (0,  0);
+    uint64_t key = 0x0E329232EA6D0D73;
+
+    std::vector<uint64_t> data = e.encryptData(a,key);
+
+    int i = 0;
+    for (uint64_t c: data) {
+        EXPECT_EQ (c,  ciphertext[i]);
+        i++;
+    }
+
 }
 
 TEST(fullDecryptionTest, test1) {
 
+    std::vector<uint64_t> ciphertext;
+    ciphertext.push_back(0xC0999FDDE378D7ED);
+    ciphertext.push_back(0x727DA00BCA5A84EE); 
+    ciphertext.push_back(0x47F269A4D6438190); 
+    ciphertext.push_back(0xD9D52F78F5358499); // // typo in documentation
+    ciphertext.push_back(0x828AC9B453E0E653);
+
+    // TODO: add zero padding
+    std::string a = "Your lips are smoother than vaseline\r\n";
+
     DesEncryption e;
-    EXPECT_EQ (0,  0);
+
+    uint64_t key = 0x0E329232EA6D0D73;
+    std::string out = e.decryptData(ciphertext,key);
+
+    EXPECT_EQ (out,  a);
 }
